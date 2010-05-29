2e5cee86fd624b36ce62aaaa021195d75e161a83
Michael Barton <mike@weirdlooking.com>
2010-05-29 21:32:12 +0000

    better setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+try:
+    from Cython.Distutils import build_ext
+    source = ['libeventhub.pyx']
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    source = ['libeventhub.c']
+    cmdclass = {}
 
-ext_modules = [Extension("libeventhub", ["libeventhub.pyx"])]
+hub_module = Extension('libeventhub', source, libraries=['event'])
 
-setup(
-    name = 'libeventhub',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules
-)
+setup(name='libeventhub', cmdclass=cmdclass, ext_modules=[hub_module])
 
