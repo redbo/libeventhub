bd4a69b35ab7a7f6a38cdfe0b748ccad6d2a0ad1
Mike Barton <mike@weirdlooking.com>
2011-04-13 07:20:10 +0000

    better nbio

--- nbio.pyx
+++ nbio.pyx
@@ -5,6 +5,9 @@ import os
 from eventlet.hubs import trampoline
 
 
+THREAD_COUNT = 32
+
+
 cdef extern from 'fcntl.h':
     int posix_fadvise(int fd, long offset, long len, int advice) nogil
     int posix_fallocate(int fd, long offset, long len) nogil
@@ -20,11 +23,18 @@ cdef extern from 'unistd.h':
 
 
 cdef extern from 'pthread.h':
+    ctypedef long pthread_mutex_t
+    void pthread_mutex_init(pthread_mutex_t *, void *arg) nogil
+    void pthread_mutex_lock(pthread_mutex_t *) nogil
+    void pthread_mutex_unlock(pthread_mutex_t *) nogil
     ctypedef long pthread_t
     ctypedef long pthread_attr_t
     int pthread_create(pthread_t *thread, pthread_attr_t *attr,
                 void *(*start_routine)(void*), void *arg) nogil
     int pthread_join(pthread_t thread, void **value_ptr) nogil
+    int pthread_attr_init(pthread_attr_t *attr)
+    int pthread_attr_setdetachstate(pthread_attr_t *attr, int)
+    int PTHREAD_CREATE_DETACHED
 
 
 cdef struct fd_operation:
@@ -37,40 +47,67 @@ cdef struct fd_operation:
     int response_writer
 
 
-cdef void *fd_operate(fd_operation *op) nogil:
+cdef int queue_fd_reader, queue_fd_writer
+cdef pthread_mutex_t reader_mutex
+cdef pthread_attr_t reader_mutex_attr
+
+
+cdef _launch_thread():
+    cdef pthread_t io_thread
+    cdef pthread_attr_t attr
+    pthread_attr_init(&attr)
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)
+    pthread_create(&io_thread, NULL, &fd_operate, NULL)
+
+
+def _init():
+    global queue_fd_reader, queue_fd_writer
+    queue_fd_reader, queue_fd_writer = os.pipe()
+    pthread_mutex_init(&reader_mutex, NULL)
+    for x in xrange(THREAD_COUNT):
+        _launch_thread()
+
+
+cdef void *fd_operate(void *arg) nogil:
     """
     pthread target that performs file operations.
     No python allowed, so we never have to acquire the GIL.
     """
-    if op.op == 1:
-        op.response = write(op.fd, op.buf, op.length)
-    elif op.op == 2:
-        op.response = read(op.fd, op.buf, op.length)
-    elif op.op == 3:
-        op.response = fsync(op.fd)
-    elif op.op == 4:
-        op.response = fdatasync(op.fd)
-    elif op.op == 5:
-        op.response = posix_fallocate(op.fd, 0, op.length)
-    elif op.op == 6:
-        op.response = posix_fadvise(op.fd, op.offset, op.length,
-                POSIX_FADV_DONTNEED)
-    close(op.response_writer)
+    cdef fd_operation *op
+    while 1:
+        pthread_mutex_lock(&reader_mutex)
+        read(queue_fd_reader, &op, sizeof(fd_operation *))
+        pthread_mutex_unlock(&reader_mutex)
+        if op.op == 1:
+            op.response = write(op.fd, op.buf, op.length)
+        elif op.op == 2:
+            op.response = read(op.fd, op.buf, op.length)
+        elif op.op == 3:
+            op.response = fsync(op.fd)
+        elif op.op == 4:
+            op.response = fdatasync(op.fd)
+        elif op.op == 5:
+            op.response = posix_fallocate(op.fd, 0, op.length)
+        elif op.op == 6:
+            op.response = posix_fadvise(op.fd, op.offset, op.length,
+                    POSIX_FADV_DONTNEED)
+        write(op.response_writer, "!", 1)
 
 
-cdef _file_op(int file_op, int fd, char *buf=NULL, long length=0, long offset=0):
-    cdef fd_operation op
+cdef _file_op(int opcode, int fd, char *buf=NULL, long length=0, long offset=0):
+    cdef fd_operation op, *opp
+    opp = &op
     cdef pthread_t thrd
     response_reader, op.response_writer = os.pipe()
     op.fd = fd
-    op.op = file_op
+    op.op = opcode
     op.buf = buf
     op.length = length
     op.offset = offset
-    pthread_create(&thrd, NULL, <void *(*)(void*)>&fd_operate, <void *>&op)
+    write(queue_fd_writer, &opp, sizeof(fd_operation*))
     trampoline(response_reader, read=True)
     close(response_reader)
-    pthread_join(thrd, NULL)
+    close(op.response_writer)
     return op.response
 
 # TODO raise errors
@@ -102,6 +139,10 @@ def disk_fallocate(fd, length):
 def disk_drop_cache(fd, offset, length):
     return _file_op(6, fd, NULL, length, offset)
 
+
 __all__ = ['disk_write', 'disk_read', 'disk_fsync', 'disk_fdatasync',
            'disk_fallocate', 'disk_drop_cache']
+
+
+_init()
 
--- speedtest.py
+++ speedtest.py
@@ -1,39 +1,14 @@
-"""
-This is pretty normal for me:
-
-redbo@swiftdev:~/libeventhub$ python speedtest.py 
-normal read          0.182
-libeventhub          3.069
-tpool                31.296
-"""
 import os
 import time
 
-import eventlet
-from eventlet import tpool, hubs
-
-import libeventhub
-import nbio
-
-
-hubs.use_hub(libeventhub)
-
-READ_AMOUNT = (1024 ** 3)
-
-def read_file(mode):
+def read_file():
     start = time.time()
     fd = os.open('/dev/zero', 0)
     read_amount = 0
-    while read_amount < READ_AMOUNT:
-        if mode == 0:
-            chunk = os.read(fd, 65536)
-        elif mode == 1:
-            chunk = tpool.execute(os.read, fd, 65536)
-        elif mode == 2:
-            chunk = nbio.disk_read(fd, 65536)
-        read_amount += len(chunk)
-    print "%5.3f seconds" % (time.time() - start)
+    while read_amount < (1024 ** 3): # 1gb
+        read_amount += len(os.read(fd, 32768)) # 32kb
+    print (time.time() - start), "seconds"
 
-for x in xrange(5):
-    read_file(2)
+for x in xrange(3):
+    read_file()
 
